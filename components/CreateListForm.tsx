c41ec6f1188f0d1b6359c328bffe132177b5e0da
Eric Nichols <ebn646@gmail.com>
2023-08-06 15:54:55 -0400

    added open close for create list form

--- components/CreateListForm.tsx
+++ components/CreateListForm.tsx
@@ -21,41 +21,35 @@ function CreateListForm() {
     <div>
       {
         open ? (
-          <p>I am open</p>
-        ) : <p>I am closed</p>
-      }
-      {/* Open */}
-      <div className="bg-slate-300 p-2 shrink-0 w-72 rounded overflow-hidden">
-        <input
-          type="text"
-          placeholder="Enter list title"
-          className="w-full py-3 px-2 rounded text-xs border-slate-400 outline-blue-500 mb-2"
-          name="title"
-          onChange={(event) => handleChangeEvent(event)}
-
-        />
-        <div className="flex items-center">
-          <button
-            onClick={createList}
-            className="px-3 py-3 rounded font-sans text-xs font-semibold outline-black mr-2 bg-blue-700 text-white"
-          >
-            Add list
-          </button>
-          <button
-            onClick={() => setOpen(false)}
-          >
-            X
-          </button>
+          <div className="bg-slate-300 p-2 shrink-0 w-72 rounded overflow-hidden">
+            <input
+              type="text"
+              placeholder="Enter list title"
+              className="w-full py-3 px-2 rounded text-xs border-slate-400 outline-blue-500 mb-2"
+              name="title"
+              onChange={(event) => handleChangeEvent(event)}
+            />
+            <div className="flex items-center">
+              <button
+                onClick={createList}
+                className="px-3 py-3 rounded font-sans text-xs font-semibold outline-black mr-2 bg-blue-700 text-white"
+              >
+                Add list
+              </button>
+              <button
+                onClick={() => setOpen(false)}
+              >
+                X
+              </button>
+            </div>
+          </div>
+        ) : <div
+          onClick={() => setOpen(true)}
+          className="flex items-center px-4 h-full transition-all duration-200 bg-blue-500 hover:bg-blue-400 py-2 cursor-pointer rounded-md">
+          <MdAdd color="white" size={24} />
+          <p className="ml-2 text-xs text-white">Create new list</p>
         </div>
-      </div>
-
-      {/* Closed */}
-      <div
-        onClick={() => setOpen(true)}
-        className="flex items-center px-4 h-full transition-all duration-200 bg-blue-500 hover:bg-blue-400 py-2 cursor-pointer rounded-md">
-        <MdAdd color="white" size={24} />
-        <p className="ml-2 text-xs text-white">Create new list</p>
-      </div>
+      }
     </div>
   )
 }
