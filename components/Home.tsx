f802df0d77b166a18cc0990c4d32e8683ca94aaf
Eric Nichols <ebn646@gmail.com>
2023-08-22 17:55:14 -0400

    home page header update

--- components/Home.tsx
+++ components/Home.tsx
@@ -3,9 +3,9 @@ import { signIn } from 'next-auth/react'
 import Image from 'next/image'
 const Home = () => {
   return (
-    <div className="flex max-w-4xl mx-auto">
+    <div className="flex max-w-6xl py-10 mx-auto">
       <div className="flex flex-col flex-1">
-        <h1 className="text-5xl font-bold">Trello brings all your tasks, teammates, and tools together</h1>
+        <h1 className="text-5xl font-bold">Trello AI brings all your tasks, teammates, and tools together</h1>
         <button 
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-10"
         onClick={() => {
@@ -18,6 +18,7 @@ const Home = () => {
       </div>
       <div className="flex flex-1">
         <Image
+          className="object-cover"
           src="/images/TrelloUICollage_4x.webp"
           alt="Picture of the author"
           width={500}
