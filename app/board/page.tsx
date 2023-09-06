ac2ac466726a5a9820167a53e7db6a7bd6c8afa5
Eric Nichols <ebn646@gmail.com>
2023-09-05 20:58:10 -0400

    added async user to board page

--- app/board/page.tsx
+++ app/board/page.tsx
@@ -1,20 +1,19 @@
-'use client'
 import Board from '@/components/board/Board'
 import Header from '@/components/Header'
 // Remember you must use an AuthProvider for 
 // client components to useSession
-import { useSession } from 'next-auth/react'
+import { getAuthSession } from '@/utils/auth'
 import { redirect } from 'next/navigation'
 
-export default function ClientPage({ params }: { params: { id: string } }) {
-  const { id } = params
 
-  const { data: session } = useSession({
-    required: true,
-    onUnauthenticated() {
-      redirect('/')
-    }
-  })
+export default async function ClientPage({ params }: { params: { id: string } }) {
+  const session = await getAuthSession()
+  console.log(session?.user)
+  if (!session?.user) {
+    redirect('/')
+  }
+
+
 
   return (
     <section className="flex flex-col h-full">
